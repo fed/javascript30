e981a34bfb342ad5ab34e72b5a93c48843aa6e80
Federico Knüssel <fknussel@gmail.com>
2017-01-30 14:35:26 -0300

    Add template file

---
+++ template.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <link href="style.css" rel="stylesheet" />
+</head>
+<body>
+
+
+  <!-- Logic -->
+  <script>
+
+  </script>
+
+</body>
+</html>
